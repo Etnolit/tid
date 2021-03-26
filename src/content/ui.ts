5b1850d8fa6720453271abe717063af1fa87a34f
Linus Törngren <linus@etnolit.se>
2021-03-26 14:31:03 +0100

    Refactored for easier testing and higher coverage.

--- src/content/ui.ts
+++ src/content/ui.ts
@@ -1,9 +1,14 @@
-
+import { browser } from 'webextension-polyfill-ts'
 export class DOMClock {
-    clock: HTMLElement
-    digits: HTMLElement[]
+    private clock: HTMLElement
+    private digits: HTMLElement[]
 
-    constructor() {
+    private timer: number
+    private end: number
+    
+    constructor(timestamp: number) {
+        this.end = timestamp
+        
         const clock = document.createElement('div')
         clock.id = 'clock'
 
@@ -38,9 +43,44 @@ export class DOMClock {
         rightNumbers.appendChild(this.digits[0])
 
         this.clock = clock
+        this.timer = window.setInterval(() => this.update(), 100)
+
+        this.update()
     }
 
-    public setDigit(index: number, value: number): void {
+    private update(now?: number): void {
+        if (!now) now = Date.now()
+        const timeLeft = Math.floor((this.end - now) / 1000)
+    
+        if (timeLeft < 0) {
+            this.flash()
+    
+            // Request notification
+            const sending = browser.runtime.sendMessage({type: 'notify'})
+    
+//            const handleResponse = (message?: any) => console.log(message)
+            const handleError = (reason: any) => console.log(reason)
+    
+            sending.catch(handleError)
+
+            window.clearInterval(this.timer)
+        } else {
+            // Calculate and update numbers on screen
+            const s = timeLeft % 60
+            const m = Math.floor(timeLeft / 60)
+        
+    
+            this.setDigit(0, s % 10)
+            this.setDigit(1, Math.floor(s / 10))
+            this.setDigit(2, m % 10)
+            this.setDigit(3, Math.floor(m / 10) % 10)
+            this.setDigit(4, m >= 100 ? Math.floor(m / 100) % 10 : -1)
+            this.setDigit(5, m >= 1000 ? Math.floor(m / 1000) % 10 : -1)
+        }
+    
+    }
+
+    private setDigit(index: number, value: number): void {
         if (value >= 0) {
             this.digits[index].innerText = value.toString()
         } else {
@@ -48,11 +88,11 @@ export class DOMClock {
         }
     }
 
-    public render(): HTMLElement{
+    public render(): HTMLElement {
         return this.clock
     }
 
-    public flash(): void {
+    private flash(): void {
         this.clock.classList.add('flash')
     }
 }
@@ -62,7 +102,14 @@ export class DOMBar {
     bar: HTMLElement
     innerBar: HTMLElement
 
-    constructor() {
+    private timer: number
+    private end: number
+    private duration: number
+
+    constructor(timestamp: number, duration: number) {
+        this.end = timestamp
+        this.duration = duration
+
         const bar = document.createElement('div')
         bar.id = 'bar'
 
@@ -73,9 +120,17 @@ export class DOMBar {
 
         this.bar = bar
         this.innerBar = innerBar
+
+        this.timer = window.setInterval(() => this.update(Date.now()), 100)
     }
 
-    public setPosition(position: number): void {
+    public update(now: number): void {
+        const timeLeft = Math.floor((this.end - now) / 1000)
+
+        this.setPosition(timeLeft / (this.duration / 1000) * 100)
+    }
+
+    private setPosition(position: number): void {
         this.innerBar.style.width = `${position}%`
     }
 
